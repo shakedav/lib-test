5cb0598771e12e053247a09acd54ca65487c3ad4
shakedav <shakedav@gmail.com>
2021-12-23 17:42:01 +0200

    test

---
+++ dist/my-lib/lib/my-lib.service.d.ts
@@ -0,0 +1,7 @@
+import * as i0 from "@angular/core";
+export declare class MyLibService {
+    constructor();
+    test(): void;
+    static ɵfac: i0.ɵɵFactoryDeclaration<MyLibService, never>;
+    static ɵprov: i0.ɵɵInjectableDeclaration<MyLibService>;
+}
